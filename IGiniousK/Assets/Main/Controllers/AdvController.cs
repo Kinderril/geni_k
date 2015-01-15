180f7e16fbd0e902126137721064f6bc7653e54d
Kinderril <kinderril@gmail.com>
2015-01-16 00:14:41 +0400

    add new ad

--- IGiniousK/Assets/Main/Controllers/AdvController.cs
+++ IGiniousK/Assets/Main/Controllers/AdvController.cs
@@ -13,20 +13,30 @@ public class AdvController
         Advertisement.Initialize("23042", true);
         Log("Advertisement " + Advertisement.isReady());
        // Chartboost.cacheMoreApps(CBLocation.GameOver);
+        // Your Publisher ID is: 47205add69ea9390660aa634f662514f
+        HeyzapAds.start("47205add69ea9390660aa634f662514f", HeyzapAds.FLAG_NO_OPTIONS);
+      //  HeyzapAds.
     }
 
     public void ShowAdv()
     {
+        
         Log("Advertisement " + Advertisement.isReady());
         if (Advertisement.isReady())
         {
             Advertisement.Show();
         }
+
     }
 
     public void ShowAdvCB()
     {
-        Log("Chartboost " );
+        Log("HZInterstitialAd " + HZInterstitialAd.isAvailable());
+        if (HZInterstitialAd.isAvailable())
+        {
+            //HZInterstitialAd.fetch();
+            HZInterstitialAd.show();
+        }
        // Chartboost.showInterstitial(CBLocation.GameOver);
     }
 
